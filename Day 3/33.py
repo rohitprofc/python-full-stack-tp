ca5e7dd256ec320fb354a6a1b8bb5c78ecab994e
rohitprofc <rohitprofc@gmail.com>
2024-07-05 17:09:16 +0530

    3-7-2024

--- Day 3/33.py
+++ Day 3/33.py
@@ -2,6 +2,6 @@
 
 import numpy as np
 a=np.array([1,2,3,'re']) 
-#taking all the elemnts as the strings as array only contain homogenous elements
+#taking all the elements as the strings as array only contain homogenous elements
 print(a)
 print(a.dtype)
